2362d59a65b3885d4bf4f3267fb5eaabe6b90416
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-05-05 16:26:20 -0400

    pequeño detalle

--- portafolio/src/App.tsx
+++ portafolio/src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages
-import Home from "./pages/Home"
-import ProjectDetail from "./pages/ProjectDetail"
-import NotFound from "./pages/NotFound"
+import Home from "./pages/Home";
+import ProjectDetail from "./pages/ProjectDetail";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/projects/:id" element={<ProjectDetail/>}/>
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
