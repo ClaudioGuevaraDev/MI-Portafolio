ff5669da27957d1d0182ad5b9b24b471ae9a4a76
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-06-30 11:12:00 -0400

    No recuerdo lo último que hice

--- portafolio/src/App.tsx
+++ portafolio/src/App.tsx
@@ -2,6 +2,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-loading"
 import LoadingScreen from "./components/LoadingScreen";
 
+import "@fontsource/raleway/300.css";
+import "@fontsource/raleway/400.css";
+import "@fontsource/raleway/500.css";
+import "@fontsource/raleway/700.css";
+
 // Pages
 import Home from "./pages/Home";
 
